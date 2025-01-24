62581da5da813895e881af16a3a503ae95b14925
akshat05ag <05akshatagarwal@gmail.com>
2025-01-24 20:30:10 +0530

    changes committed

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,16 +1,16 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'x-rapidapi-key': '8e9f291db8msh0377100633ff1f1p1b1d48jsn4a0838c0f239',
+    'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
   },
 };
 
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+    'x-rapidapi-key': '8e9f291db8msh0377100633ff1f1p1b1d48jsn4a0838c0f239',
+    'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com',
   },
 };
 
